91aa10198b847e7acacd047227bd5c1087fab04a
jozanza <hello@jsavary.com>
2025-07-18 01:11:48 -0400

    update TurboProgramMetadata to use BTreeSet for channels and commands fields

--- turbo-genesis-abi/src/metadata.rs
+++ turbo-genesis-abi/src/metadata.rs
@@ -1,18 +1,20 @@
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+use std::collections::BTreeSet;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
 pub struct TurboProgramMetadata {
     pub name: String,
     pub program_id: String,
     pub owner_id: String,
-    pub commands: Vec<TurboProgramCommandMetadata>,
-    pub channels: Vec<TurboProgramChannelMetadata>,
+    pub commands: BTreeSet<TurboProgramCommandMetadata>,
+    pub channels: BTreeSet<TurboProgramChannelMetadata>,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
 pub struct TurboProgramCommandMetadata {
     pub name: String,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
 pub struct TurboProgramChannelMetadata {
     pub name: String,
 }
