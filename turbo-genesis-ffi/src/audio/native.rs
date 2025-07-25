9fa9115127a5faf8cda6413164b3599fc45b8e1a
Valerie Roche <val.nuccio@gmail.com>
2025-07-25 16:19:33 -0600

    add native pub fn

--- turbo-genesis-ffi/src/audio/native.rs
+++ turbo-genesis-ffi/src/audio/native.rs
@@ -19,3 +19,5 @@ pub fn get_volume(key_ptr: *const u8, key_len: u32) -> f32 {
 }
 
 pub fn set_volume(key_ptr: *const u8, key_len: u32, decibels: f32) {}
+
+pub fn set_panning(key_ptr: *const u8, key_len: u32, panning: f32) {}
