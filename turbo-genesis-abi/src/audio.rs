8ab65833b04a0213280d593c2ee8714d8b35aa67
Valerie Roche <val.nuccio@gmail.com>
2025-10-04 00:35:05 +0900

    lose keys

--- turbo-genesis-abi/src/audio.rs
+++ turbo-genesis-abi/src/audio.rs
@@ -3,27 +3,9 @@
 
 use borsh::{BorshDeserialize, BorshSerialize};
 
-#[derive(Debug)]
 
-/// Based on winit KeyCode which is based on the w3c UI Events spec.
-/// See [`KeyboardEvent.code`](https://w3c.github.io/uievents-code/#code-value-tables)
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, BorshDeserialize)]
-pub enum TurboSoundSettingKey {
-    LoopRegionStart,
-    LoopRegionEnd,
-    Volume,
-}
-
-#[derive(Clone, BorshSerialize, BorshDeserialize, Debug)]
+#[derive(Clone, BorshSerialize, BorshDeserialize)]
 pub struct TurboSoundSetting {
-    /// Unique name/key for the sound
-    pub name: String,
-    pub loop_region: [f64; 2],
-    pub volume: f32,
-    pub panning: f32,
-}
-
-pub struct Keys {
     pub loop_region: [f64; 2],
     pub volume: f32,
     pub panning: f32,
@@ -38,10 +20,9 @@ pub struct KeySetting {
 impl TurboSoundSetting {
     pub fn new() -> Self {
         Self {
-            name: "".to_owned(),
             loop_region: [0.0, 0.0],
-            volume: 100.0,
-            panning: 0,
+            volume: 1.0,
+            panning: 0.0,
         }
     }
 }
